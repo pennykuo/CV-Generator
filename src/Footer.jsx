54cef5577b03e1a9705d0b9aa5b173342287e019
GETACAD\penny.kuo <penny.kuo@getac.cm.tw>
2024-03-13 16:22:09 +0800

    delete test

--- src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-        <footer id="footer">
-            <p>Made by:Penny Kuo</p>
-        </footer>
-
-    )
-}
--- src/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return(
-        <header id ="header">
-            <h1>CV Generator</h1>
-        </header>
-    )
-}
